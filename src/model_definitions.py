17e54dca70a320b75387ef13122b023f73b2667d
nexhua <CChristofear@hotmail.com>
2023-06-05 19:05:26 +0300

    added cnn versions

--- src/model_definitions.py
+++ src/model_definitions.py
@@ -12,12 +12,26 @@ def get_generators(model: Model):
         test_generator = tf.keras.preprocessing.image.ImageDataGenerator(
             rescale=1./255)
         return data_generator, test_generator
+    elif(model.name == 'cnn_v1'):
+        data_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+            rescale=1./255, validation_split=model.validation_split)
+        test_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+            rescale=1./255)
+        return data_generator, test_generator
+    elif(model.name == 'cnn_v2'):
+        data_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+            rescale=1./255, validation_split=model.validation_split)
+        test_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+            rescale=1./255)
+        return data_generator, test_generator
     else:
         return None, None
 
 
 def get_model(model: Model):
+
     if(model.name == 'simple_v1'):
+        print(model.name)
         model = models.Sequential()
 
         model.add(layers.Conv2D(32, (3, 3), activation="relu",
@@ -29,6 +43,48 @@ def get_model(model: Model):
 
         model.add(layers.Flatten())
         model.add(layers.Dense(128, activation='relu'))
+        model.add(layers.Dense(101, activation='softmax'))
+
+        return model
+    elif(model.name == 'cnn_v1'):
+        print(model.name)
+        model = models.Sequential()
+
+        model.add(layers.Conv2D(32, (3, 3), activation="relu",
+                  input_shape=(256, 256, 3)))
+        model.add(layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+
+        model.add(layers.Conv2D(64, (3, 3), activation="relu"))
+        model.add(layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+
+        model.add(layers.Conv2D(128, (3, 3), activation="relu"))
+        model.add(layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+
+        model.add(layers.Flatten())
+        model.add(layers.Dense(1024, activation="relu"))
+        model.add(layers.Dense(512, activation="relu"))
+        model.add(layers.Dense(101, activation='softmax'))
+
+        return model
+    elif(model.name == 'cnn_v2'):
+
+        model = models.Sequential()
+
+        model.add(layers.Conv2D(32, (3, 3), activation="relu",
+                  input_shape=(256, 256, 3)))
+        model.add(layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+
+        model.add(layers.Conv2D(64, (3, 3), activation="relu"))
+        model.add(layers.Conv2D(64, (3, 3), activation="relu"))
+        model.add(layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(layers.Dropout(0.4))
+
+        model.add(layers.Conv2D(128, (3, 3), activation="relu"))
+        model.add(layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(layers.Dropout(0.2))
+
+        model.add(layers.GlobalAveragePooling2D())
+        model.add(layers.Dense(512, activation="relu"))
         model.add(layers.Dense(101, activation='softmax'))
 
         return model
--- src/models.py
+++ src/models.py
@@ -2,14 +2,29 @@ from model import Model
 from model_definitions import get_generators, get_model
 
 simple_v1 = Model(name='simple_v1',
-                  validation_split=0.2, 
-                  batch_size=32, 
-                  optimizer='adam', 
-                  loss='categorical_crossentropy', 
-                  metrics=['accuracy'], 
+                  validation_split=0.2,
+                  batch_size=32,
+                  optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'],
                   epochs=5)
 
 simple_v1.model = get_model(simple_v1)
 data_generator, test_generator = get_generators(simple_v1)
 simple_v1.data_generator = data_generator
 simple_v1.test_generator = test_generator
+
+
+cnn_v1 = Model(name='cnn_v1', validation_split=0.2, batch_size=32, optimizer='adam',
+               loss='categorical_crossentropy', metrics=['accuracy'], epochs=8)
+cnn_v1.model = get_model(cnn_v1)
+data_generator, test_generator = get_generators(cnn_v1)
+cnn_v1.data_generator = data_generator
+cnn_v1.test_generator = test_generator
+
+cnn_v2 = Model(name='cnn_v2', validation_split=0.2, batch_size=32, optimizer='adam',
+               loss='categorical_crossentropy', metrics=['accuracy'], epochs=8)
+cnn_v2.model = get_model(cnn_v2)
+data_generator, test_generator = get_generators(cnn_v2)
+cnn_v2.data_generator = data_generator
+cnn_v2.test_generator = test_generator
