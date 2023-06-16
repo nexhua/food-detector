da414773cd87068bc8f917477723b5b572b823bf
nexhua <CChristofear@hotmail.com>
2023-06-16 09:58:06 +0300

    some fixes

--- src/model_definitions.py
+++ src/model_definitions.py
@@ -24,6 +24,25 @@ def get_generators(model: Model):
         test_generator = tf.keras.preprocessing.image.ImageDataGenerator(
             rescale=1./255)
         return data_generator, test_generator
+    elif(model.name == 'cnn_v3'):
+        data_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+            rescale=1./255, validation_split=model.validation_split)
+        test_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+            rescale=1./255)
+        return data_generator, test_generator
+    elif(model.name == 'fine_tuned_v1'):
+        data_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+            rescale=1./255, validation_split=model.validation_split)
+        test_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+            rescale=1./255)
+        return data_generator, test_generator
+    elif(model.name == 'fine_tuned_v2'):
+        data_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+            rescale=1./255, rotation_range=20, shear_range=0.2, zoom_range=0.2, height_shift_range=0.1,
+            width_shift_range=0.1, validation_split=model.validation_split)
+        test_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+            rescale=1./255)
+        return data_generator, test_generator
     else:
         return None, None
 
@@ -31,7 +50,6 @@ def get_generators(model: Model):
 def get_model(model: Model):
 
     if(model.name == 'simple_v1'):
-        print(model.name)
         model = models.Sequential()
 
         model.add(layers.Conv2D(32, (3, 3), activation="relu",
@@ -47,7 +65,6 @@ def get_model(model: Model):
 
         return model
     elif(model.name == 'cnn_v1'):
-        print(model.name)
         model = models.Sequential()
 
         model.add(layers.Conv2D(32, (3, 3), activation="relu",
@@ -67,7 +84,6 @@ def get_model(model: Model):
 
         return model
     elif(model.name == 'cnn_v2'):
-
         model = models.Sequential()
 
         model.add(layers.Conv2D(32, (3, 3), activation="relu",
@@ -88,5 +104,52 @@ def get_model(model: Model):
         model.add(layers.Dense(101, activation='softmax'))
 
         return model
-    else:
+    elif(model.name == 'cnn_v3'):
+        model = models.Sequential()
+
+        model.add(layers.Conv2D(32, (3, 3), activation="relu",
+                  input_shape=(256, 256, 3)))
+        model.add(layers.Conv2D(32, (3, 3), activation="relu"))
+        model.add(layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+
+        model.add(layers.Conv2D(64, (3, 3), activation="relu"))
+        model.add(layers.Conv2D(64, (3, 3), activation="relu"))
+        model.add(layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+
+        model.add(layers.Conv2D(128, (3, 3), activation="relu"))
+        model.add(layers.Conv2D(128, (3, 3), activation="relu"))
+        model.add(layers.Conv2D(128, (3, 3), activation="relu"))
+        model.add(layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+
+        model.add(layers.GlobalAveragePooling2D())
+        model.add(layers.Dense(512, activation="relu"))
+        model.add(layers.Dense(256, activation="relu"))
+        model.add(layers.Dense(101, activation='softmax'))
+
         return model
+    elif(model.name == 'fine_tuned_v1'):
+        model = models.Sequential()
+
+        res_net_50 = tf.keras.applications.ResNet50V2(
+            include_top=False, input_shape=(256, 256, 3))
+
+        res_net_50.trainable = False
+        model.add(res_net_50)
+        model.add(layers.GlobalAveragePooling2D())
+        model.add(layers.Dense(101, activation='softmax'))
+
+        return model
+    elif(model.name == 'fine_tuned_v2'):
+        model = models.Sequential()
+
+        res_net_50 = tf.keras.applications.ResNet50V2(
+            include_top=False, input_shape=(256, 256, 3))
+
+        res_net_50.trainable = False
+        model.add(res_net_50)
+        model.add(layers.GlobalAveragePooling2D())
+        model.add(layers.Dense(101, activation='softmax'))
+
+        return model
+    else:
+        return None
--- src/models.py
+++ src/models.py
@@ -28,3 +28,25 @@ cnn_v2.model = get_model(cnn_v2)
 data_generator, test_generator = get_generators(cnn_v2)
 cnn_v2.data_generator = data_generator
 cnn_v2.test_generator = test_generator
+
+
+cnn_v3 = Model(name='cnn_v3', validation_split=0.2, batch_size=32, optimizer='adam',
+               loss='categorical_crossentropy', metrics=['accuracy'], epochs=10)
+cnn_v3.model = get_model(cnn_v3)
+data_generator, test_generator = get_generators(cnn_v3)
+cnn_v3.data_generator = data_generator
+cnn_v3.test_generator = test_generator
+
+fine_tuned_v1 = Model(name='fine_tuned_v1', validation_split=0.2, batch_size=32, optimizer='adam',
+               loss='categorical_crossentropy', metrics=['accuracy'], epochs=5)
+fine_tuned_v1.model = get_model(fine_tuned_v1)
+data_generator, test_generator = get_generators(fine_tuned_v1)
+fine_tuned_v1.data_generator = data_generator
+fine_tuned_v1.test_generator = test_generator
+
+fine_tuned_v2 = Model(name='fine_tuned_v2', validation_split=0.2, batch_size=32, optimizer='adam',
+               loss='categorical_crossentropy', metrics=['accuracy'], epochs=5)
+fine_tuned_v2.model = get_model(fine_tuned_v2)
+data_generator, test_generator = get_generators(fine_tuned_v2)
+fine_tuned_v2.data_generator = data_generator
+fine_tuned_v2.test_generator = test_generator
